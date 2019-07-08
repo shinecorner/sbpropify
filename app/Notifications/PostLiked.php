e7416c20728bc9c967ff1b214658e4932a07c989
Gabriel Vasile <vasile.gabriel@email.com>
2019-07-08 20:54:24 +0300

    Add PostLiked by tenant notification
    https://trello.com/c/I0ACgTMl/262-email-notifications-template-manager

---
+++ app/Notifications/PostLiked.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Post;
+use App\Models\Tenant;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Str;
+
+class PostLiked extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected $post;
+    protected $liker;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Post $post, Tenant $liker)
+    {
+        $this->post = $post;
+        $this->liker = $liker;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        if ($notifiable->settings->news_notification) {
+            return ['database', 'mail'];
+        }
+
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        // TODO: replace this with templated email
+        return (new MailMessage)
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'post_id' => $this->post->id,
+            'tenant' => $this->liker->name,
+            'fragment' => Str::limit($this->post->content, 128),
+        ];
+    }
+
+    public function toDatabase($notifiable)
+    {
+        return $this->toArray($notifiable);
+    }
+}
