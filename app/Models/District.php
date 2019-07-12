bd3ea26ef34250d634d939e7fce0fbc32d952f6f
fastcodemaker <zeynalyan_davit.mail.ru>
2019-07-12 18:01:18 +0500

    save

--- app/Models/District.php
+++ app/Models/District.php
@@ -68,6 +68,17 @@ class District extends Model
         'description' => 'string'
     ];
 
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($district) {
+            $district->district_format = $district->getUniqueIDFormat($district->id);
+            $district->save();
+        });
+    }
+
+
     public function propertyManagers()
     {
         return $this->belongsToMany(PropertyManager::class, 'district_property_manager', 'district_id', 'property_manager_id');
