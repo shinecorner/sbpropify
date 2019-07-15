1eaf87be562ba753602358850ab9f4235633a45d
fastcodemaker <zeynalyan_davit.mail.ru>
2019-07-15 11:16:22 +0500

    add migrations

--- app/Models/PropertyManager.php
+++ app/Models/PropertyManager.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\UniqueIDFormat;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
@@ -66,7 +67,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  */
 class PropertyManager extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, UniqueIDFormat;
 
     public $table = 'property_managers';
 
@@ -102,6 +103,7 @@ class PropertyManager extends Model
         'slogan',
         'xing_url',
         'linkedin_url',
+        'property_manager_format'
     ];
 
     protected $dates = ['deleted_at'];
