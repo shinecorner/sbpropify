46cd4668092ae914c373913a0a4fd47dfec8a41e
Arman Gulbangyan <armangyulbangyan1991@gmail.com>
2019-07-31 14:46:39 +0400

    fix tenant save as after unit

--- database/seeds/TenantsTableSeeder.php
+++ database/seeds/TenantsTableSeeder.php
@@ -12,6 +12,8 @@ use Illuminate\Database\Seeder;
 
 class TenantsTableSeeder extends Seeder
 {
+    use \Traits\TimeTrait;
+
     /**
      * Run the database seeds.
      *
@@ -36,6 +38,22 @@ class TenantsTableSeeder extends Seeder
 
             $data = factory(App\Models\Tenant::class)->make()->toArray();
             $date = $this->getRandomTime();
+
+            if ($email == 'tenant@example.com' || rand(0, 1)) {
+                $unit = $units->random();
+                $building = $unit->building;
+                $data['address_id'] = $building->address_id;
+                $data['building_id'] = $building->id;
+                $data['unit_id'] = $unit->id;
+                $data['status'] = Tenant::StatusActive;
+                $date = $this->getRandomTime($unit->created_at);
+
+                if ($email == 'tenant@example.com') {
+//                    $services = ServiceProvider::select('id')->limit(4)->inRandomOrder()->get();
+//                    $building->serviceProviders()->attach($services);
+                }
+            }
+
             $userData = [
                 'name' =>  sprintf('%s %s', $data['first_name'], $data['last_name']),
                 'password' => bcrypt($email),
@@ -53,20 +71,6 @@ class TenantsTableSeeder extends Seeder
 
             $data['user_id'] = $user->id;
             $data['title'] = $user->title;
-
-            if ($email == 'tenant@example.com' || rand(0, 1)) {
-                $unit = $units->random();
-                $building = $unit->building;
-                $data['address_id'] = $building->address_id;
-                $data['building_id'] = $building->id;
-                $data['unit_id'] = $unit->id;
-                $data['status'] = Tenant::StatusActive;
-
-                if ($email == 'tenant@example.com') {
-//                    $services = ServiceProvider::select('id')->limit(4)->inRandomOrder()->get();
-//                    $building->serviceProviders()->attach($services);
-                }
-            }
 
             $data = array_merge($data, $this->getDateColumns($date));
             $tenant = factory(App\Models\Tenant::class)->create($data);
