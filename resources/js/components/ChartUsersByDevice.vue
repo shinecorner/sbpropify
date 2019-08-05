40f8ed1030126ec6a474f2c7ad01438dcdff2220
ruslankharakhashyan <ruslankharakhashyan@gmail.com>
2019-08-05 21:09:29 +0300

    finished dashboard tenants by device chart

--- resources/js/components/ChartUsersByDevice.vue
+++ resources/js/components/ChartUsersByDevice.vue
@@ -5,9 +5,13 @@
                 <apexchart type="donut" :options="chartOptions" :series="series" />
             </el-col>
         </el-row>
-        <el-row type="flex">
-            <el-col :span="8">
-                
+        <el-row type="flex" class="legend-container">
+            <el-col :span="8" v-for="(item, index) in yData" :key="index">
+                <div class="custom-legend">
+                  <img :src="icons[index]" />
+                  <p>{{ xData[index] }}</p>
+                  <p v-if="sum > 0">{{ Math.round(item * 1000 / sum) / 10 + '%' }}</p>
+                </div>
             </el-col>
         </el-row>
     </div>
@@ -18,11 +22,21 @@ import axios from '@/axios';
 
 import chartMixin from '../mixins/adminDashboardChartMixin';
 
+import iconDesktop from '../../img/desktop.png';
+import iconTablet from '../../img/tablet.png';
+import iconMobile from '../../img/mobile.png';
+
 export default {
   components: {
     'apexchart': VueApexCharts
   },
   mixins: [chartMixin()],
+  data() {
+    return {
+      icons: [iconDesktop, iconTablet, iconMobile],
+      sum: 0,
+    }
+  },
   props: {
     colNum: {
         type: Number
@@ -33,7 +47,7 @@ export default {
       let responsive = [];
       if (this.colNum == 2) {
         responsive = [{
-          breakpoint: 1000,
+          breakpoint: 900,
           options: {
             chart: {
               width: '100%',
@@ -44,7 +58,7 @@ export default {
       }
       else {
         responsive = [{
-          breakpoint: 1500,
+          breakpoint: 1200,
           options: {
             chart: {
               width: '100%',
@@ -62,7 +76,7 @@ export default {
         chart:{
           toolbar: this.toolbar,
           width: '100%',
-          height: 270
+          height: 230
         },
         colors: ['#218BFB', '#84BFFD', '#B5D8FD'],
         plotOptions: {
@@ -71,40 +85,57 @@ export default {
               size: '80%'
             }
           }
+        },
+        dataLabels:{
+          enabled: false,
         }
       }
     }
   },
   methods: {
-      fetchData() {
-          let that = this;                                               
-          let url = 'admin/chartLoginDevice';
+    fetchData() {
+      let that = this;                                               
+      let url = 'admin/chartLoginDevice';
 
-          return axios.get(url)
-          .then(function (response) {
-              that.yData = response.data.data.data.map(val => parseFloat(val) || 0);
-              that.xData = response.data.data.labels;
-          }).catch(function (error) {
-              console.log(error);
-          })
-      }
+      return axios.get(url)
+      .then(function (response) {
+        that.yData = response.data.data.data.map(val => parseFloat(val) || 0);
+        that.xData = response.data.data.labels;
+        that.sum = that.yData.reduce(function(a, b) { return a + b; }, 0);
+      }).catch(function (error) {
+        console.log(error);
+      })
+    }
   }
 }
 </script>
 <style lang="scss">
-    .chart-card {
-        .tenants-by-device-chart .apexcharts-canvas {
-            margin-top: 25px;
-            @media screen and (max-width: 1650px) {
-              margin-top: 30px;
-            }
-        }
-        .tenants-by-device-chart {
-            position: relative;
-
-            .apexcharts-canvas {
-                position: unset;
-            }
-        }
+  .chart-card {
+    .tenants-by-device-chart .apexcharts-canvas {
+      margin-top: 25px;
+      @media screen and (max-width: 1650px) {
+        margin-top: 30px;
+      }
     }
+    .tenants-by-device-chart {
+      position: relative;
+
+      .apexcharts-canvas {
+          position: unset;
+      }
+
+      .legend-container {
+        max-width: 300px;
+        margin-left: auto;
+        margin-right: auto;
+      }
+      .custom-legend {
+        text-align: center;
+
+        p {
+          margin: 3px;
+        }
+      }
+    }
+  }
 </style>
