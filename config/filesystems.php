4de392eac4b0361ccc957e4da967eab993520e5d
Arman Gulbangyan <armangyulbangyan1991@gmail.com>
2019-08-06 13:20:41 +0400

    generalize filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -1,6 +1,6 @@
 <?php
 
-return [
+$config = [
 
     /*
     |--------------------------------------------------------------------------
@@ -62,27 +62,6 @@ return [
             'visibility' => 'public',
         ],
 
-        'buildings_house_rules' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public/buildings/house_rules'),
-            'url' => env('APP_URL').'/storage/buildings/house_rules',
-            'visibility' => 'public',
-        ],
-
-        'buildings_operating_instructions' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public/buildings/operating_instructions'),
-            'url' => env('APP_URL').'/storage/buildings/operating_instructions',
-            'visibility' => 'public',
-        ],
-
-        'buildings_other' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public/buildings/other'),
-            'url' => env('APP_URL').'/storage/buildings/other',
-            'visibility' => 'public',
-        ],
-
         'posts_media' => [
             'driver' => 'local',
             'root' => storage_path('app/public/posts/media'),
@@ -128,3 +107,16 @@ return [
     ],
 
 ];
+
+$buildingCategories = \App\Models\Building::BuildingMediaCategories;
+
+foreach ($buildingCategories  as $category) {
+    $config['disks']['1buildings_' . $category] = [
+        'driver' => 'local',
+        'root' => storage_path('app/public/buildings/' . $category),
+        'url' => env('APP_URL').'/storage/buildings/' . $category,
+        'visibility' => 'public',
+    ];
+}
+
+return $config;
