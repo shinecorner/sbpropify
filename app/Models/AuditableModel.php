026af6c4eca5a637713d5bf31723714589676579
Arman Gulbangyan <armangyulbangyan1991@gmail.com>
2019-08-07 14:50:22 +0400

    fixes

--- app/Models/AuditableModel.php
+++ app/Models/AuditableModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use OwenIt\Auditing\Contracts\Auditable;
 
-class AuditableMocdel extends \Illuminate\Database\Eloquent\Model implements Auditable
+class AuditableModel extends \Illuminate\Database\Eloquent\Model implements Auditable
 {
     use \OwenIt\Auditing\Auditable;
 
