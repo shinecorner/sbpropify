ec5a9c0372b48c31434870e9be5905c8eba20720
Ciprian Lazar <ciprian.lazar@expremio.com>
2019-08-09 17:10:06 +0300

    Fix functional tests for Address

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,33 @@
 <?php
+declare(strict_types=1);
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
+/**
+ * Class TestCase
+ * @package Tests
+ */
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * @var User
+     */
+    var $user;
+
+    /**
+     *
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        if (!$this->user) {
+            $this->user = (new User())->first();
+        }
+    }
 }
