1ebe7e096e206804f572bbd304d4117372bc70d3
Ylber Muhaxheri <ym@gocaution.ch>
2019-08-11 13:18:36 +0000

    common.php edited online with Bitbucket

--- resources/lang/de/common.php
+++ resources/lang/de/common.php
@@ -9,21 +9,21 @@ return [
     //Request
     'request_status_1' => 'Erhalten',
     'request_status_2' => 'In Bearbeitung',
-    'request_status_3' => 'Avisiert',
+    'request_status_3' => 'Anvisiert',
     'request_status_4' => 'Erledigt',
     'request_status_5' => 'Reaktiviert',
     'request_status_6' => 'Archiviert',
 
     'originalRequest_status_1' => 'Erhalten',
     'originalRequest_status_2' => 'In Bearbeitung',
-    'originalRequest_status_3' => 'Avisiert',
+    'originalRequest_status_3' => 'Anvisiert',
     'originalRequest_status_4' => 'Erledigt',
     'originalRequest_status_5' => 'Reaktiviert',
     'originalRequest_status_6' => 'Archiviert',
 
     //Email
-    'email_footer_message1' => 'Diese automatisierte Nachricht wurde für :UserName erstellt.',
-    'email_footer_message2' => 'Sie erhalten Sie Mitteilung, da Sie Nutzer von :CompanyName sind.',
+    'email_footer_message1' => 'Sie erhalten Sie Mitteilung, weil Sie Nutzer vom Mieterportal der :CompanyName sind.',
+    'email_footer_message2' => '...',
     'email_link_contacts' => 'Kontakt',
     'email_link_terms_of_use' => 'Nutzungsbedingungen',
     'email_link_data_protection' => 'Datenschutz',
