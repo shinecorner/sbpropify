d630dc8cd191eedde647fc24e99b15919a5b4cf4
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-08-22 18:26:00 +0400

    fix migration

--- database/migrations/2019_08_22_123425_fill_request_assignees_table.php
+++ database/migrations/2019_08_22_123425_fill_request_assignees_table.php
@@ -23,10 +23,22 @@ class FillRequestAssigneesTable extends Migration
             ]);
         }
         $requestAssigners = \Illuminate\Support\Facades\DB::table('request_assignee')->get();
+        $users = \App\Models\User::whereIn('id', $requestAssigners->pluck('user_id'))->with('propertyManager')->get()->keyBy('id');
+
         foreach ($requestAssigners as $requestAssigner) {
+            $user = $users[$requestAssigner->user_id] ?? null;
+
+            if (empty($user) || empty($user->propertyManager)) {
+                \Illuminate\Support\Facades\DB::table('request_assignee')->where([
+                    'request_id' => $requestAssigner->request_id,
+                    'user_id' => $requestAssigner->user_id,
+                ])->delete();
+                continue;
+            }
+
             \App\Models\ServiceRequestAssignee::create([
                 'request_id' => $requestAssigner->request_id,
-                'assignee_id' => $requestAssigner->user_id,
+                'assignee_id' => $user->propertyManager->id,
                 'assignee_type' => array_flip(\Illuminate\Database\Eloquent\Relations\Relation::$morphMap)[\App\Models\PropertyManager::class] ?? 'manager',
                 'created_at' => now()
             ]);
