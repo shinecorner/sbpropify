df06dda89650b5867267f035bafbf0c27d74153f
TigerDevloper <tigerdeveloper@outlook.com>
2019-08-26 06:03:26 -0500

    Search loaded api issue fix

--- resources/js/components/AssignmentByType.vue
+++ resources/js/components/AssignmentByType.vue
@@ -94,7 +94,7 @@
         created() {
             this.resetToAssignList();
             this.assign();
-            this.remoteSearch();
+            //this.remoteSearch();
         }
     }
 </script>
