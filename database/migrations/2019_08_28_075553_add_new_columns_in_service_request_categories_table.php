f89ead9b1a59e21521f64ecab6bde010c7036bc3
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-08-28 12:03:51 +0400

    add migration

---
+++ database/migrations/2019_08_28_075553_add_new_columns_in_service_request_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNewColumnsInServiceRequestCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('service_request_categories', function (Blueprint $table) {
+            $table->string('name_it')->after('name');
+            $table->string('name_fr')->after('name');
+            $table->string('name_de')->after('name');
+            $table->tinyInteger('room')->after('name_it')->nullable();
+            $table->tinyInteger('location')->after('room')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('service_request_categories', function (Blueprint $table) {
+            $table->dropColumn('name_it', 'name_fr', 'name_de', 'room', 'location');
+        });
+    }
+}
