6f0ce401b2d176d6156a3527095ba2b661215a20
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-08-30 12:38:57 +0400

    generalize uploadFile

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -218,40 +218,6 @@ class PostRepository extends BaseRepository
     }
 
     /**
-     * @param string $collectionName
-     * @param string $dataBase64
-     * @param Post $model
-     * @return bool|\Spatie\MediaLibrary\Models\Media
-     * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded
-     * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded\InvalidBase64Data
-     */
-    public function uploadFile(string $collectionName, string $dataBase64, Post $model)
-    {
-        if (!$data = base64_decode($dataBase64)) {
-            return false;
-        }
-
-        $file  = finfo_open();
-        $mimeType  = finfo_buffer($file, $data, FILEINFO_MIME_TYPE);
-        finfo_close($file);
-
-        if (!isset($this->mimeToExtension[$mimeType])){
-            return false;
-        }
-        $extension = $this->mimeToExtension[$mimeType];
-
-        $diskName = sprintf("posts_%s", $collectionName);
-
-        $media = $model->addMediaFromBase64($dataBase64)
-            ->sanitizingFileName(function ($fileName) use ($extension) {
-                return sprintf('%s.%s', str_slug($fileName), $extension);
-            })
-            ->toMediaCollection($collectionName, $diskName);
-
-        return $media;
-    }
-
-    /**
      * @param Tenant $tenant
      * @return Post|bool|mixed
      * @throws \Prettus\Validator\Exceptions\ValidatorException
--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -100,30 +100,4 @@ class ProductRepository extends BaseRepository
         }
         Notification::send($users, new ProductPublished($product));
     }
-
-    public function uploadFile(string $collectionName, string $dataBase64, Product $model)
-    {
-        if (!$data = base64_decode($dataBase64)) {
-            return false;
-        }
-
-        $file  = finfo_open();
-        $mimeType  = finfo_buffer($file, $data, FILEINFO_MIME_TYPE);
-        finfo_close($file);
-
-        if (!isset($this->mimeToExtension[$mimeType])){
-            return false;
-        }
-        $extension = $this->mimeToExtension[$mimeType];
-
-        $diskName = sprintf("products_%s", $collectionName);
-
-        $media = $model->addMediaFromBase64($dataBase64)
-            ->sanitizingFileName(function ($fileName) use ($extension) {
-                return sprintf('%s.%s', str_slug($fileName), $extension);
-            })
-            ->toMediaCollection($collectionName, $diskName);
-
-        return $media;
-    }
 }
