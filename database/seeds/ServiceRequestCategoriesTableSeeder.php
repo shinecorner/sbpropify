25f2f6be840fcadcf2e1bddc3ec30100f1e20fb1
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-08-30 21:13:25 +0400

    fix seeder for single call

--- database/seeds/ServiceRequestCategoriesTableSeeder.php
+++ database/seeds/ServiceRequestCategoriesTableSeeder.php
@@ -14,6 +14,7 @@ class ServiceRequestCategoriesTableSeeder extends Seeder
     public function run()
     {
         Schema::disableForeignKeyConstraints();
+        DB::table('service_request_categories')->truncate();
         DB::unprepared(file_get_contents(database_path('sql' . DIRECTORY_SEPARATOR . 'service_request_categories.sql')));
         Schema::enableForeignKeyConstraints();
     }
