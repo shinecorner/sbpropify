d395dc7cccf4302205bfeda58b8bfd4dbd3743cc
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-09-02 17:28:02 +0400

    assignees avatar

--- app/Transformers/ServiceRequestAssigneeTransformer.php
+++ app/Transformers/ServiceRequestAssigneeTransformer.php
@@ -25,8 +25,9 @@ class ServiceRequestAssigneeTransformer extends BaseTransformer
             'item_id' => $model->assignee_id,
             'edit_id' => $model->assignee_id,
             'type' => $model->assignee_type,
-            'email' => $model->related->email ?? 'incorrect email',
-            'name' => $model->related->name ?? 'incorrect relation'
+            'email' => $model->related ? $model->related->email : 'incorrect relation',
+            'name' => $model->related ? $model->related->name : 'incorrect relation',
+            'avatar' => $model->related ? $model->related->avatar : 'incorrect relation'
         ];
 
         return $response;
