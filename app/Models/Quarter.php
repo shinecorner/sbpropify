485e14989ed4c74f51e09a98b7f0a229db291ade
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-09-04 02:58:15 +0400

    rename district to quarter

---
+++ app/Models/Quarter.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UniqueIDFormat;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @SWG\Definition(
+ *      definition="Quarter",
+ *      required={"name", "description"},
+ *      @SWG\Property(
+ *          property="id",
+ *          description="id",
+ *          type="integer",
+ *          format="int32"
+ *      ),
+ *      @SWG\Property(
+ *          property="name",
+ *          description="name",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="description",
+ *          description="description",
+ *          type="string"
+ *      ),
+ *      @SWG\Property(
+ *          property="created_at",
+ *          description="created_at",
+ *          type="string",
+ *          format="date-time"
+ *      ),
+ *      @SWG\Property(
+ *          property="updated_at",
+ *          description="updated_at",
+ *          type="string",
+ *          format="date-time"
+ *      )
+ * )
+ */
+class Quarter extends AuditableModel
+{
+    use SoftDeletes, UniqueIDFormat;
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        'name' => 'required|string',
+    ];
+    public $table = 'quarters';
+    public $fillable = [
+        'name',
+        'description',
+        'quarter_format'
+    ];
+    protected $dates = ['deleted_at'];
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'name' => 'string',
+        'description' => 'string',
+        'quarter_format' => 'string'
+    ];
+
+    public function propertyManagers()
+    {
+        return $this->belongsToMany(PropertyManager::class, 'quarter_property_manager', 'quarter_id', 'property_manager_id');
+    }
+}
