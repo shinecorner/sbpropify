8425a0a6bef7e6b8dd1546da6bdb8e6d3cdf7510
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-09-04 01:34:32 +0400

    return also district

--- app/Transformers/QuarterTransformer.php
+++ app/Transformers/QuarterTransformer.php
@@ -24,6 +24,7 @@ class QuarterTransformer extends BaseTransformer
             'id' => $model->id,
             'name' => $model->name,
             'quarter_format' => $model->quarter_format,
+            'district_format' => $model->quarter_format,
             'description' => $model->description,
         ];
 
