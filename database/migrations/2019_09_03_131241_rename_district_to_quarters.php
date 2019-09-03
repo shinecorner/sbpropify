3db35b7897680e81346d8c54d7edf4197959da34
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-09-03 17:21:55 +0400

    rename ditrict related tables

---
+++ database/migrations/2019_09_03_131241_rename_district_to_quarters.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameDistrictToQuarters extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::rename('districts', 'quarters');
+        Schema::rename('district_property_manager', 'quarters_property_manager');
+        Schema::rename('district_service_provider', 'quarters_service_provider');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::rename('quarters', 'districts');
+        Schema::rename('quarters_property_manager', 'district_property_manager');
+        Schema::rename('quarters_service_provider', 'district_service_provider');
+    }
+}
