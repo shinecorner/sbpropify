0e619e0b565824f25f82f7bb0edb15075d411971
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-09-04 01:31:28 +0400

    fix quarter transformer

---
+++ app/Transformers/QuarterTransformer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Models\Quarter;
+
+/**
+ * Class QuarterTransformer.
+ *
+ * @package namespace App\Transformers;
+ */
+class QuarterTransformer extends BaseTransformer
+{
+    /**
+     * Transform the QuarterT entity.
+     *
+     * @param \App\Models\Quarter $model
+     *
+     * @return array
+     */
+    public function transform(Quarter $model)
+    {
+        $response = [
+            'id' => $model->id,
+            'name' => $model->name,
+            'quarter_format' => $model->quarter_format,
+            'description' => $model->description,
+        ];
+
+        return $response;
+    }
+}
