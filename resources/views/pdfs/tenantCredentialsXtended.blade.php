a6ebf1a5e46775d8c730634711580d1137c52923
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-09-03 16:22:06 +0400

    pdf

--- resources/views/pdfs/tenantCredentialsXtended.blade.php
+++ resources/views/pdfs/tenantCredentialsXtended.blade.php
@@ -7,68 +7,89 @@
 @section('body')
 <table>
   <tr>
-    <td class="logo"><img src="{{ asset('images/logo3.png') }}" /></td>
+    <td class="tenant">
+      <p>
+        @lang("tenant.dear")
+        <br>
+        <b>{{ $tenant->first_name . ' ' . $tenant->last_name }}</b>
+      </p>
+
+      @if ($tenant->address)
+       <p>{{ $tenant->address->street_nr }}, {{ $tenant->address->street }} {{ $tenant->address->city }}</p>
+      @endif
+    </td>
     <td class="contact">
-        <p>{{ $re->name }}</p>
-        <p>{{ $re->address->street_nr }}, {{ $re->address->street }}<br />
+      <img src="{{ asset('images/logo3.png') }}" />
+      <p>{{ $re->name }}</p>
+      <p>{{ $re->address->street_nr }}, {{ $re->address->street }}<br />
         {{ $re->address->city }}, {{ $re->address->state->name }}</p>
-        <p>T:{{ $re->phone }}; E:{{ $re->email }}</p>
+      <p>
+        @lang('tenant.telephone'): {{ $re->phone }}
+        <br>
+        @lang('tenant.email'): {{ $re->email }}</p>
+    </td>
+  </tr>
+</table>
+
+<table>
+  <tr>
+    <td colspan="4">
+      @lang('tenant.born'), {{$tenant->birth_date ? $tenant->birth_date->format('d.m.Y') : ''}}
+      <br>
+      <br>
+      <br>
+      <b>@lang('tenant.welcome') {{ $re->name }}</b>
+      <br>
+      <br>
+      @lang('tenant.dear_sir') {{ $tenant->last_name }}
+      <br>
+      <br>
+      @lang('tenant.content_1')
+      <br>
+      <br>
+      <b>@lang('tenant.offer')</b>
+      <ul>
+        <li>@lang('tenant.offer_1')</li>
+        <li>@lang('tenant.offer_2')</li>
+        <li>@lang('tenant.offer_3')</li>
+        <li>@lang('tenant.offer_4')</li>
+        <li>@lang('tenant.offer_5')</li>
+      </ul>
+
+      <b>@lang('tenant.register')</b>
+      <br>
+      <br>
+      @lang('tenant.content_2')
+      <br>
+      <br>
     </td>
   </tr>
   <tr>
-    <td colspan=2><hr class="separator" /></td>
-  </tr>
-</table>
-
-<table>
-  <tr><th><h2>{{ __("tenant.tenancy_details") }}</h2></th></tr>
-  @if ($tenant->unit)
-  <tr>
-    <td class="detail-key">{{ __("tenant.unit") }}</td>
-    <td>{{ $tenant->unit->name }}</td>
+    <td colspan="1">{{ __("tenant.link_application") }}</td>
+    <td colspan="3">{{ url('/') }}</td>
   </tr>
   <tr>
-    <td class="detail-key">{{ __("tenant.floor") }}</td>
-    <td>{{ $tenant->unit->floor }}</td>
-  </tr>
-  @endif
-  @if ($tenant->building)
-  <tr>
-    <td class="detail-key">{{ __("tenant.building") }}</td>
-    <td>{{ $tenant->building->name }}</td>
-  </tr>
-  @endif
-  @if ($tenant->address)
-  <tr>
-    <td class="detail-key">{{ __("tenant.address") }}</td>
-    <td>{{ $tenant->address->street_nr }}, {{ $tenant->address->street }} {{ $tenant->address->city }}</td>
-  </tr>
-  @endif
-  @if ($tenant->rent_start)
-  <tr>
-    <td class="detail-key">{{ __("tenant.rent_start") }}</td>
-    <td>{{ $tenant->rent_start->format('Y-m-d')}}</td>
-  </tr>
-  @endif
-</table>
-
-<table>
-  <tr><th><h2>{{ __("tenant.login_credentials") }}</h2></th></tr>
-  <tr>
-    <td class="detail-key">{{ __("tenant.website") }}</td>
-    <td>{{ url('/') }}</td>
+    <td colspan="1">{{ __("tenant.code") }}</td>
+    <td colspan="3">{{ $code }}</td>
   </tr>
   <tr>
-    <td class="detail-key">{{ __("tenant.username") }}</td>
-    <td>{{ $tenant->user->email }}</td>
+    <td colspan="1">{{ __("tenant.your_email") }}</td>
+    <td colspan="3">{{ $tenant->user->email }}</td>
   </tr>
   <tr>
-    <td class="detail-key">{{ __("tenant.url") }}</td>
-    <td>{{ $url }}</td>
-  </tr>
-  <tr>
-    <td class="detail-key">{{ __("tenant.code") }}</td>
-    <td>{{ $code }}</td>
+    <td colspan="4">
+      <br>
+      @lang('tenant.content_3')
+      <br>
+      <br>
+      @lang('tenant.content_4')
+      <br>
+      <br>
+      <br>
+      @lang('tenant.your_sincerely')
+      <br>
+      @lang('tenant.your_administration')
+    </td>
   </tr>
 </table>
 @endsection
