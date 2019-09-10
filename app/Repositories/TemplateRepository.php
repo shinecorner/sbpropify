e74ad9a71f7c89f226f2ce142258186e88f94848
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-09-10 13:51:40 +0400

    fix bg color email link

--- app/Repositories/TemplateRepository.php
+++ app/Repositories/TemplateRepository.php
@@ -169,8 +169,9 @@ class TemplateRepository extends BaseRepository
     function button($url, $text)
     {
         $linkClass = 'button button-primary';
-        $linkStyle = 'font-family: -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, Helvetica, Arial, sans-serif, \'Apple Color Emoji\', \'Segoe UI Emoji\', \'Segoe UI Symbol\'; box-sizing: border-box; border-radius: 3px; box-shadow: 0 2px 3px rgba(0, 0, 0, 0.16); color: #FFF; display: inline-block; text-decoration: none; -webkit-text-size-adjust: none; background-color: #3490DC; border-top: 10px solid #3490DC; border-right: 18px solid #3490DC; border-bottom: 10px solid #3490DC; border-left: 18px solid #3490DC;';
-
+        $bgColor = $this->realEstate->primary_color ?? '#3490DC';
+        $linkStyle = 'font-family: -apple-system, BlinkMacSystemFont, \'Segoe UI\', Roboto, Helvetica, Arial, sans-serif, \'Apple Color Emoji\', \'Segoe UI Emoji\', \'Segoe UI Symbol\'; box-sizing: border-box; border-radius: 3px; box-shadow: 0 2px 3px rgba(0, 0, 0, 0.16); color: #FFF; display: inline-block; text-decoration: none; -webkit-text-size-adjust: none; background-color: {color}; border-top: 10px solid {color}; border-right: 18px solid {color}; border-bottom: 10px solid {color}; border-left: 18px solid {color};';
+        $linkStyle = str_replace('{color}', $bgColor, $linkStyle);
         return sprintf('<a class="%s" style="%s" href="%s">%s</a>', $linkClass, $linkStyle, $url, $text);
     }
 
