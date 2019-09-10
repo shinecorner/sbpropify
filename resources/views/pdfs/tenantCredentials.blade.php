b6c470c5beadda7365f967f6eb96454908ec6bfc
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-09-10 19:44:40 +0400

    bulk pdf

--- resources/views/pdfs/tenantCredentials.blade.php
+++ resources/views/pdfs/tenantCredentials.blade.php
@@ -5,8 +5,67 @@
 @endsection
 
 @section('body')
-    <p>@lang("models.tenant.credentials_pdf.url", [], $language): <a href="{{ url('/') }}">{{ url('/') }}</a></p>
-    <p>@lang("models.tenant.credentials_pdf.username", [], $language): {{ $tenant->user->email }}</p>
-    <p>@lang("models.tenant.credentials_pdf.url", [], $language): {{ $url }}</p>
-    <p>@lang("models.tenant.credentials_pdf.code", [], $language): {{ $code }}</p>
+    <table>
+        <tr>
+            <td colspan="2">
+                <br>
+                <br>
+                <br>
+                {{ $re->address->city }}, {{now()->format('d.m.Y')}}
+                <br>
+                <br>
+                <br>
+                <b>@lang('models.tenant.credentials_pdf.welcome', [], $language) {{ $re->name }}</b>
+                <br>
+                <br>
+                @if(\App\Models\Tenant::TitleCompany == $tenant->title)
+                    @lang('general.pdf_salutation.' . $tenant->title, [], $language)
+                @else
+                    @lang('general.pdf_salutation.' . $tenant->title, ['name' => $tenant->last_name], $language)
+                @endif
+                <br>
+                <br>
+                @lang('models.tenant.credentials_pdf.content_1', [], $language)
+                <br>
+                <br>
+                <p class="offer"><b>@lang('models.tenant.credentials_pdf.offer', [], $language)</b></p>
+                <ul class="offer">
+                    {!! __('models.tenant.credentials_pdf.offers', [], $language) !!}
+                </ul>
+
+                <b>@lang('models.tenant.credentials_pdf.register', [], $language)</b>
+                <br>
+                @lang('models.tenant.credentials_pdf.content_2', [], $language)
+                <br>
+                <br>
+            </td>
+        </tr>
+        <tr>
+            <td>@lang("models.tenant.credentials_pdf.link_application", [], $language)</td>
+            <td>{{ url('/activate') }}</td>
+        </tr>
+        <tr>
+            <td>{{ __("models.tenant.credentials_pdf.code") }}</td>
+            <td>{{ $code }}</td>
+        </tr>
+        <tr>
+            <td>{{ __("models.tenant.credentials_pdf.your_email") }}</td>
+            <td>{{ $tenant->user->email }}</td>
+        </tr>
+        <tr>
+            <td colspan="4">
+                <br>
+                @lang('models.tenant.credentials_pdf.content_3', [], $language)
+                <br>
+                <br>
+                @lang('models.tenant.credentials_pdf.content_4', [], $language)
+                <br>
+                <br>
+                <br>
+                @lang('models.tenant.credentials_pdf.your_sincerely', [], $language)
+                <br>
+                @lang('models.tenant.credentials_pdf.your_administration', [], $language)
+            </td>
+        </tr>
+    </table>
 @endsection
