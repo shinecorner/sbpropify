f9b38ebe21074c0f88902951aaaef7ff469e25f1
Vitaly <vetal.klim94@gmail.com>
2019-09-12 11:41:32 +0300

    yimo-vue-editor in admin templates edit

--- resources/js/views/Admin/Templates/Edit.vue
+++ resources/js/views/Admin/Templates/Edit.vue
@@ -22,13 +22,7 @@
                         </el-form-item>
                         <el-form-item :label="$t('models.template.body')" :prop="`translations.${language}.body`"
                                       :rules="validationRules.body">
-                            <vue-editor
-                                          :editor-options="toolbarOptions"
-                                          @text-change="updateValue"
-                                          ref="editor"
-                                          v-model="model.translations[language].body"
-                            >
-                            </vue-editor>
+                            <yimo-vue-editor v-model="model.translations[language].body"></yimo-vue-editor>
                         </el-form-item>
                     </card>
                 </el-col>
@@ -72,12 +66,8 @@
     import EditActions from 'components/EditViewActions';
     import SelectLanguage from 'components/SelectLanguage';
     import TemplatesMixin from 'mixins/adminTemplatesMixin';
+    import YimoVueEditor from 'yimo-vue-editor';
 
-    let VueEditor, Quill;
-    if (process.browser) {
-        VueEditor = require("vue2-editor").VueEditor;
-        Quill = require("vue2-editor").Quill;
-    }
 
     export default {
         mixins: [TemplatesMixin({
@@ -87,77 +77,13 @@
             Heading,
             Card,
             EditActions,
-            VueEditor,
+            YimoVueEditor,
             SelectLanguage
         },
         data() {
             return {
-                toolbarOptions: {
-                    modules: {
-                        toolbar: {
-                            container: [
-                                [{ font: [] }],
-                                [{ header: [false, 1, 2, 3, 4, 5, 6] }],
-                                ["bold", "italic", "underline", "strike"],
-                                [{ align: [] }],
-                                ["blockquote", "code-block"],
-                                [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
-                                [{ indent: "-1" }, { indent: "+1" }],
-                                [{ color: [] }, { background: [] }],
-                                ["link", "image", "video"],
-                                ["clean"],
-                                ["showHtml"]
-                            ],
-                            handlers: {
-                                showHtml: () => {
-                                    if (this.txtArea.style.display === "") {
-                                        const html = this.txtArea.value;
-                                        if (html === '<p><br/></p>') {
-                                            this.html = null;
-                                        } else {
-                                            this.html = html.replace(new RegExp('<p><br/>', 'g'), '<p>')
-                                        }
-                                        this.quill.pasteHTML(html);
-                                    }
-                                    this.txtArea.style.display =
-                                        this.txtArea.style.display === "none" ? "" : "none";
-                                }
-                            }
-                        }
-                    }
-                },
-                content: "",
-                rawContent: ""
             }
         },
-        mounted() {
-            this.quill = this.$refs.editor.quill;
-            this.txtArea = document.createElement("textarea");
-            this.txtArea.style.cssText =
-                "width: 100%;margin: 0px;background: rgb(29, 29, 29);box-sizing: border-box;color: rgb(204, 204, 204);font-size: 15px;outline: none;padding: 20px;line-height: 24px;font-family: Consolas, Menlo, Monaco, &quot;Courier New&quot;, monospace;position: absolute;top: 0;bottom: 0;border: none;display:none;resize: none;";
-
-            const htmlEditor = this.quill.addContainer("ql-custom");
-            htmlEditor.appendChild(this.txtArea);
-
-            this.quill.on("text-change", (delta, oldDelta, source) => {
-                var html = this.quill.getHTML();
-                this.txtArea.value = html;
-            });
-            this.content = this.value;
-        },
-        watch: {
-            content() {
-                this.rawContent = this.content;
-            },
-            rawContent() {
-                this.content = this.rawContent;
-            }
-        },
-        methods: {
-            updateValue () {
-                this.$emit('input', this.$refs.editor.quill.getHTML())
-            }
-        }
     }
 </script>
 
@@ -227,8 +153,15 @@
         display: block !important;
     }
 
-    .ql-showHtml:after {
+    .ql-showSource:after {
         content: "[source]";
+    }
+    button.ql-showSource {
+        width: 100% !important;
+    }
+
+    .ql-showHtml:after {
+        content: "[paste HTML mode]";
     }
     button.ql-showHtml {
         width: 100% !important;
