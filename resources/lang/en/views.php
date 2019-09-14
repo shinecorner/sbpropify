8439f3e24e28b2b7fa640f86bb20d6e0d8d0ea54
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-09-14 14:07:11 +0400

    sync all lang files

--- resources/lang/en/views.php
+++ resources/lang/en/views.php
@@ -1,19 +1,15 @@
 <?php
 return [
-    'tenant' => 
-    [
-      'my' => 
-      [
-        'personal' => 
-        [
-          'title' => 'Personal data',
-          'description' => 'My personal details.',
-          'placeholder' => 
-          [
-            'title' => 'No personal data available.',
-            'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-          ],
+    'tenant' => [
+        'my' => [
+            'personal' => [
+                'title' => 'Personal data',
+                'description' => 'My personal details.',
+                'placeholder' => [
+                    'title' => 'No personal data available.',
+                    'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                ],
+            ],
         ],
-      ],
-    ], 
+    ],
 ];
