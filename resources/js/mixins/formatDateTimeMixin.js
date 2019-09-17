417f26a8fe0a7b1b851b2cd98f792b4998a85b48
TigerDevloper <tigerdeveloper@outlook.com>
2019-09-17 12:19:15 -0500

    dateformat fix

--- resources/js/mixins/formatDateTimeMixin.js
+++ resources/js/mixins/formatDateTimeMixin.js
@@ -3,15 +3,10 @@ import {format} from 'date-fns'
 export default {
     methods: {
         formatDatetime (dateTime) {
-            var res = dateTime.split(" ");
             return this.$t('general.dateTimeFormat', {
-                date: res[0],
-                time: res[1]
+                date: format(dateTime, 'DD.MM.YYYY'),
+                time: format(dateTime, 'HH:mm')
             })
-            // return this.$t('general.dateTimeFormat', {
-            //     date: format(dateTime, 'DD.MM.YYYY'),
-            //     time: format(dateTime, 'HH:mm')
-            // })
         }
     }
 }
