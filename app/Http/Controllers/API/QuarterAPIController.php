b78995d1bc689916fedd6fc356f7227bedff72c8
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-09-18 17:11:39 +0400

    assign managers and user to quarter

--- app/Http/Controllers/API/QuarterAPIController.php
+++ app/Http/Controllers/API/QuarterAPIController.php
@@ -3,12 +3,16 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\AppBaseController;
+use App\Http\Requests\API\Quarter\BatchAssignManagers;
+use App\Http\Requests\API\Quarter\BatchAssignUsers;
 use App\Http\Requests\API\Quarter\CreateRequest;
 use App\Http\Requests\API\Quarter\UpdateRequest;
 use App\Http\Requests\API\Quarter\ListRequest;
 use App\Http\Requests\API\Quarter\ViewRequest;
 use App\Http\Requests\API\Quarter\DeleteRequest;
+use App\Models\PropertyManager;
 use App\Models\Quarter;
+use App\Models\User;
 use App\Repositories\QuarterRepository;
 use App\Transformers\QuarterTransformer;
 use Illuminate\Http\Request;
@@ -313,5 +317,196 @@ class QuarterAPIController extends AppBaseController
             return $this->sendError(__('models.quarter.errors.deleted') . $e->getMessage());
         }
         return $this->sendResponse($ids, __('models.quarter.deleted'));
+    }
+
+    /**
+     * @param int $id
+     * @param Request $request
+     * @return Response
+     *
+     * @SWG\Get(
+     *      path="/quarters/{id}/assignees",
+     *      summary="Get a listing of the Quarter assignees.",
+     *      tags={"Quarter"},
+     *      description="Get a listing of the Quarter assignees.",
+     *      produces={"application/json"},
+     *      @SWG\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @SWG\Schema(
+     *              type="object",
+     *              @SWG\Property(
+     *                  property="success",
+     *                  type="boolean"
+     *              ),
+     *              @SWG\Property(
+     *                  property="data",
+     *                  type="array",
+     *                  @SWG\Items(ref="#/definitions/QuarterAssignee")
+     *              ),
+     *              @SWG\Property(
+     *                  property="message",
+     *                  type="string"
+     *              )
+     *          )
+     *      )
+     * )
+     */
+    public function getAssignees(int $id, Request $request)
+    {
+        // @TODO permissions
+        $quarter = $this->quarterRepository->findWithoutFail($id);
+        if (empty($quarter)) {
+            return $this->sendError(__('models.quarter.errors.not_found'));
+        }
+
+        $perPage = $request->get('per_page', env('APP_PAGINATE', 10));
+        $assignees = $quarter->assignees()->paginate($perPage);
+        $assignees = $this->getAssigneesRelated($assignees, [PropertyManager::class, User::class]);
+
+        $response = (new QuarterAssigneeTransformer())->transformPaginator($assignees) ;
+        return $this->sendResponse($response, 'Assignees retrieved successfully');
+    }
+
+    /**
+     * @param int $id
+     * @param BatchAssignManagers $request
+     * @return Response
+     *
+     * @SWG\Post(
+     *      path="/quarters/{id}/managers",
+     *      summary="Assign the provided propertyManagers to the Quarter",
+     *      tags={"Quarter"},
+     *      description=" <a href='http://dev.propify.ch/api/docs#/Quarter/post_quarters__id__managers'>http://dev.propify.ch/api/docs#/Quarter/post_quarters__id__managers</a>",
+     *      produces={"application/json"},
+     *      @SWG\Parameter(
+     *          name="managerIds",
+     *          description="ids of managers",
+     *          type="array",
+     *          required=true,
+     *          in="query",
+     *          @SWG\Items(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @SWG\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @SWG\Schema(
+     *              type="object",
+     *              @SWG\Property(
+     *                  property="success",
+     *                  type="boolean"
+     *              ),
+     *              @SWG\Property(
+     *                  property="data",
+     *                  ref="#/definitions/Quarter"
+     *              ),
+     *              @SWG\Property(
+     *                  property="message",
+     *                  type="string"
+     *              )
+     *          )
+     *      )
+     * )
+     */
+    public function assignManagers(int $id, BatchAssignManagers $request)
+    {
+        /** @var Quarter $quarter */
+        $quarter = $this->quarterRepository->findWithoutFail($id);
+        if (empty($quarter)) {
+            return $this->sendError(__('models.quarter.errors.not_found'));
+        }
+
+        $managerIds = $request->get('managersIds') ?? $request->get('managerIds');
+        try {
+            $currentManagers = $quarter->propertyManagers()
+                ->whereIn('property_managers.id', $managerIds)
+                ->pluck('property_managers.id')
+                ->toArray();
+
+            $newManagers = array_diff($managerIds, $currentManagers);
+            $attachData  = [];
+            foreach ($newManagers as $manager) {
+                $attachData[$manager] = ['created_at' => now()];
+            }
+            $quarter->propertyManagers()->syncWithoutDetaching($attachData);
+        } catch (\Exception $e) {
+            return $this->sendError( __('models.quarter.errors.manager_assigned') . $e->getMessage());
+        }
+
+        $response = (new QuarterTransformer)->transform($quarter);
+        return $this->sendResponse($response, __('models.quarter.managers_assigned'));
+    }
+
+    /**
+     * @param int $id
+     * @param BatchAssignUsers $request
+     * @return Response
+     *
+     * @SWG\Post(
+     *      path="/quarters/{id}/users",
+     *      summary="Assign the provided users to the Quarter",
+     *      tags={"Quarter"},
+     *      description="Assign the provided users(administrator, super-administrator) to the Quarter",
+     *      produces={"application/json"},
+     *      @SWG\Parameter(
+     *          name="userIds",
+     *          description="ids of users",
+     *          type="array",
+     *          required=true,
+     *          in="query",
+     *          @SWG\Items(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @SWG\Response(
+     *          response=200,
+     *          description="successful operation",
+     *          @SWG\Schema(
+     *              type="object",
+     *              @SWG\Property(
+     *                  property="success",
+     *                  type="boolean"
+     *              ),
+     *              @SWG\Property(
+     *                  property="data",
+     *                  ref="#/definitions/Quarter"
+     *              ),
+     *              @SWG\Property(
+     *                  property="message",
+     *                  type="string"
+     *              )
+     *          )
+     *      )
+     * )
+     */
+    public function assignUsers(int $id, BatchAssignUsers $request)
+    {
+        /** @var Quarter $quarter */
+        $quarter = $this->quarterRepository->findWithoutFail($id);
+        if (empty($quarter)) {
+            return $this->sendError(__('models.quarter.errors.not_found'));
+        }
+
+        $userIds = $request->get('userIds');
+        try {
+            $currentUsers = $quarter->users()
+                ->whereIn('users.id', $userIds)
+                ->pluck('users.id')
+                ->toArray();
+
+            $newUsers = array_diff($userIds, $currentUsers);
+            $attachData  = [];
+            foreach ($newUsers as $userId) {
+                $attachData[$userId] = ['created_at' => now()];
+            }
+            $quarter->users()->syncWithoutDetaching($attachData);
+        } catch (\Exception $e) {
+            return $this->sendError( __('models.quarter.errors.user_assigned') . $e->getMessage());
+        }
+
+        $response = (new QuarterTransformer)->transform($quarter);
+        return $this->sendResponse($response, __('models.quarter.user_assigned'));
     }
 }
