54d21d832ba58e797bbce48f9e79ffcfb08a7fdf
klmnko <vetal.klim94@gmail.com>
2019-09-20 17:00:37 +0300

    remove uploadImg feature

--- resources/js/mixins/adminEditorConfig.js
+++ resources/js/mixins/adminEditorConfig.js
@@ -35,7 +35,6 @@ export default {
                     'table',
                     'emotion',
                     '|',
-                    'img',
                     'video',
                     'location',
                     'insertcode',
