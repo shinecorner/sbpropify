451ef066bc2c2ef47c0aa369d8e8fb072e652b57
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-09-23 16:36:01 +0400

    add new columns in service_requests for active remainders

---
+++ database/migrations/2019_09_23_122114_add_new_columns_in_service_requests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNewColumnsInServiceRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('service_requests', function (Blueprint $table) {
+            $table->smallInteger('active_reminder')->after('resolution_time')->default(0);
+            $table->integer('reminder_user_id')->unsigned()->nullable()->after('active_reminder');
+            $table->integer('days_left_due_date')->unsigned()->default(0)->after('reminder_user_id');
+            $table->foreign('reminder_user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('service_requests', function (Blueprint $table) {
+            $table->dropForeign('service_requests_reminder_user_id_foreign');
+            $table->dropColumn('active_reminder', 'days_left_due_date', 'reminder_user_id');
+        });
+    }
+}
