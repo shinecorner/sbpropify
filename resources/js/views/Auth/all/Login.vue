9f648ef74798df9016eb25f99b2d0c54c48654c8
ruslankharakhashyan <ruslankharakhashyan@gmail.com>
2019-09-24 03:11:47 +0300

    login variation

---
+++ resources/js/views/Auth/all/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <login1 v-if="this.$constants.login.variation == 1"/>
+    <login2 v-else-if="this.$constants.login.variation==2"/>
+</template>
+<script>
+    import login1 from 'views/Auth/1/Login';
+    import login2 from 'views/Auth/2/Login';
+
+    export default {
+        components: {
+            login1,
+            login2
+        }
+    }
+</script>
+<style lang="scss" scoped>
+  
+</style>
