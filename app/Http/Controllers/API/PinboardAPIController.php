dd43f3531078e870ea4363f7c7df25375e044ab6
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-09-26 18:42:31 +0400

    imp

--- app/Http/Controllers/API/PinboardAPIController.php
+++ app/Http/Controllers/API/PinboardAPIController.php
@@ -36,6 +36,7 @@ use App\Transformers\UserTransformer;
 use Carbon\Carbon;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use InfyOm\Generator\Criteria\LimitOffsetCriteria;
 
 /**
@@ -1142,5 +1143,85 @@ class PinboardAPIController extends AppBaseController
         $vs = $p->views()->with('user')->paginate($perPage);
         $ret = $pvt->transformPaginator($vs);
         return $this->sendResponse($ret, 'Views retrieved successfully');
+    }
+
+    /**
+     * @SWG\Get(
+     *      path="/pinboards/rss.xml",
+     *      summary="RSS feed of pinboards",
+     *      tags={"RSS"},
+     *      description="Get RSS feed of pinboards",
+     *      produces={"application/xml"},
+     *      @SWG\Response(
+     *          response=200,
+     *          description="successful operation"
+     *      )
+     * )
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|Response
+     */
+    public function showNewsRSS()
+    {
+        $feed = Cache::remember('rss_feed', 600, function() {
+            return file_get_contents("https://www.blick.ch/news/schweiz/rss.xml");
+        });
+
+        return response($feed, 200, [
+            'Content-Type' => 'application/xml'
+        ]);
+    }
+
+    /**
+     * @SWG\Get(
+     *      path="/pinboards/weather.json",
+     *      summary="JSON feed of weather",
+     *      tags={"weather"},
+     *      description="Get json feed of weather",
+     *      produces={"application/json"},
+     *      @SWG\Response(
+     *          response=200,
+     *          description="successful operation"
+     *      )
+     * )
+     *
+     * @param RealEstateRepository $reRepo
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|Response
+     */
+    public function showWeatherJSON(RealEstateRepository $reRepo)
+    {
+        $zip = $this->getZip($reRepo);
+        $feed = Cache::remember('weather_at_' . $zip, 60*60, function() use ($reRepo, $zip) {
+            $appid = env('OPENWEATHERMAP_API_KEY');
+            $countryCode = env('OPENWEATHERMAP_COUNTRY_CODE', 'ch');
+            $zipCountry = $zip . ',' . $countryCode;
+            $url = "http://api.openweathermap.org/data/2.5/weather?zip=" . $zipCountry . "&appid=" . $appid;
+
+            return file_get_contents($url);
+        });
+
+        return response($feed, 200, [
+            'Content-Type' => 'application/json'
+        ]);
+    }
+
+    /**
+     * @param $reRepo
+     * @return int
+     */
+    private function getZip($reRepo)
+    {
+        $u = \Auth::user();
+        if ($u->tenant && $u->tenant->address && $u->tenant->address->zip) {
+            return $u->tenant->address->zip;
+        }
+        $defaultZip = 3172;
+        $realEstate = $reRepo->first();
+        if (empty($realEstate)) {
+            return $defaultZip;
+        }
+        if (!isset($realEstate->address)) {
+            return $defaultZip;
+        }
+
+        return $realEstate->address->zip;
     }
 }
