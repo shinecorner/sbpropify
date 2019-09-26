381f482f0cac915efd8ecb418aac279c8e4fd608
Arman Gyulbangyan <armangyulbangyan1991@gmail.com>
2019-09-27 03:42:11 +0400

    imp

---
+++ database/migrations/2019_09_26_233530_fix_morph_types.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class FixMorphTypes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        update_db_fileds(\App\Models\Comment::class, ['commentable_type'], 'post', 'pinboard');
+        update_db_fileds(\App\Models\Conversation::class, ['conversationable_type'], 'post', 'pinboard');
+        update_db_fileds(\App\Models\Like::class, ['likeable_type'], 'post', 'pinboard');
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
