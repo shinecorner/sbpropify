24533b3d8a8cae86bc45fad338c0d2d24afb7ea2
Rajesh Kaswala <rajesh.starlite@gmail.com>
2019-09-28 12:15:14 +0530

    close home page

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*Route::get('/', function () {
+Route::get('/', function () {
 	return redirect('/login');
-});*/
+});
 Route::get('/{any}', function () {
     return view('index');
 })->where('any', '.*');
