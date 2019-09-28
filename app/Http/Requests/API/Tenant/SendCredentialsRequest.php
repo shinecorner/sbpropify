19b42a030547787880ae28efcc7443f239f1be8e
Rajesh Kaswala <rajesh.starlite@gmail.com>
2019-09-28 14:22:39 +0530

    revert dump send credentials email

--- app/Http/Requests/API/Tenant/SendCredentialsRequest.php
+++ app/Http/Requests/API/Tenant/SendCredentialsRequest.php
@@ -13,6 +13,6 @@ class SendCredentialsRequest extends BaseRequest
      */
     public function authorize()
     {
-        return true;//$this->can('send_credentials-tenant');
+        return $this->can('send_credentials-tenant');
     }
 }
